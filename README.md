0be7c206611719c5bc2619521374fe35cfdd0069
Jonathan Turnbull-Reilly <jonathan.d.reilly@gmail.com>
2017-05-18 12:01:31 -0400

    Filter article data for headlines, summary, thumbnail, type, and url.

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ## Assumptions
 * Use no frameworks, only vanilla js and css
-* Set a reasonable level of browser compatibility
+* Set a reasonable level of browser compatibility (IE 9, array.map, array.foreach, array.filter)
 * Deploy on GH pages
--- app.js
+++ app.js
@@ -1,19 +1,51 @@
 var NYTD = {
   endpoint: 'http://np-ec2-nytimes-com.s3.amazonaws.com/dev/test/nyregion.js',
-  getRoot: function() {
+  getRoot: function () {
     return document.getElementById('root');
   },
 
-  fetchHeadlines: function() {
+  fetchHeadlines: function () {
     var script = document.createElement('script');
     script.src = this.endpoint;
     document.body.appendChild(script);
   },
 
-  render_section_front: function(response) {
-    var root = this.getRoot();
+  // Assumption: thumbnail image is the first image in the asset's list.
+  // So far this seems to be the case.
+  findThumbnail: function (asset) {
+    if (asset.images && asset.images.length > 0) {
+      var imageTypes = asset.images[0].types;
+      var thumbNails = imageTypes.filter(function(image){
+        return image.type == 'thumbStandard';
+      });
+      return thumbNails[0];
+    }
+    return null;
+  },
+
+  filterArticleData: function (pageContent) {
+    var columns = {};
+    var _this = this;
+    pageContent.forEach(function (column) {
+      columns[column.name] = column.collections.map(function (collection) {
+        return collection.assets.map(function (asset) {
+          return {
+            headline: asset.headline,
+            summary: asset.summary,
+            thumbNail: _this.findThumbnail(asset),
+            type: asset.type,
+            url: asset.url,
+          };
+        });
+      });
+    });
+    return columns;
+  },
+
+  render_section_front: function (response) {
+    var filteredArticleData = this.filterArticleData(response.page.content);
     var headlines = document.createElement('pre');
-    headlines.innerText = JSON.stringify(response, null, 2);
+    headlines.innerText = JSON.stringify(filteredArticleData, null, 2);
     document.body.appendChild(headlines);
   }
 }
