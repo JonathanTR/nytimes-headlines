afa0da3e270d62a5af5fbeb97196a67d190e5f54
Jonathan Turnbull-Reilly <jonathan.d.reilly@gmail.com>
2017-05-18 10:37:26 -0400

    Fetch headlines data and display raw in pre element.

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
+var NYTD = {
+  endpoint: 'http://np-ec2-nytimes-com.s3.amazonaws.com/dev/test/nyregion.js',
+  getRoot: function() {
+    return document.getElementById('root');
+  },
+
+  fetchHeadlines: function() {
+    var script = document.createElement('script');
+    script.src = this.endpoint;
+    document.body.appendChild(script);
+  },
+
+  render_section_front: function(response) {
+    var root = this.getRoot();
+    var headlines = document.createElement('pre');
+    headlines.innerText = JSON.stringify(response, null, 2);
+    document.body.appendChild(headlines);
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-  var root = document.getElementById('root');
-  root.innerText = 'Hello World!'
+  NYTD.fetchHeadlines();
 });
